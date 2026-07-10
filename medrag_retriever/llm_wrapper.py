487572c41c1ed1997b93dea60a47b29b93e2e7df
Pavankumar <9553943884pavan@gmail.com>
2026-07-10 16:25:41 +0530

    Implement 4-bit quantization for model loading
    Added BitsAndBytesConfig for 4-bit quantization in model loading.

--- medrag_retriever/llm_wrapper.py
+++ medrag_retriever/llm_wrapper.py
@@ -1,11 +1,18 @@
 import torch 
 from transformers import AutoTokenizer, AutoModelForCausalLM , pipeline
 from langchain_huggingface import HuggingFacePipeline, ChatHuggingFace
+from transformers import BitsAndBytesConfig
 
 
 def build_llm(model_id: str = "microsoft/Phi-3.5-mini-instruct", max_new_tokens: int = 512, temperature: float = 0.1):
     tokenizer = AutoTokenizer.from_pretrained(model_id)
-    model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=torch.bfloat16, device_map="auto")
+    bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16,
+    bnb_4bit_use_double_quant=True
+                                  )
+    model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=torch.bfloat16, device_map="auto",quantization_config=bnb_config)
 
     hf_pipeline = pipeline(
         "text-generation",
